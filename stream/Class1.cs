b45cd10b431d38a608a2527856e6a19d0f153cd7
bohsh <bohsh@DESKTOP-ACA6CS2>
2023-04-05 10:56:30 +0300

    Добавьте файлы проекта.

---
+++ stream/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace stream
+{
+    class SaveAsThread
+    {
+        private int[] array;
+        private string fileName;
+        
+
+        public SaveAsThread(int[] array, string fileName)
+        {
+            this.array = array;
+            this.fileName = fileName;
+        }
+
+        public void SaveArrayToFile()
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                
+                foreach(int element in array)
+                {
+                    writer.WriteLine(element);
+                }
+                                       
+                    
+            }
+        }
+
+        public void Start()
+        {
+            Thread thread = new Thread(SaveArrayToFile);
+            thread.Start();
+        }
+    }
+}
---
+++ stream/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stream
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array1 = new int[] { 1, 2, 3, 5, 4 };
+            int[] array2 = new int[] { 4, 5, 6, 1, 2 };
+            int[] array3 = new int[] { 7, 8, 9, 4, 5 };
+
+            SaveAsThread saveAsThread1 = new SaveAsThread(array1, "textfile1.txt");
+            SaveAsThread saveAsThread2 = new SaveAsThread(array2, "textfile2.txt");
+            SaveAsThread saveAsThread3 = new SaveAsThread(array3, "textfile3.txt");
+            saveAsThread1.Start();
+            saveAsThread2.Start();
+            saveAsThread3.Start();
+        }
+    }
+}
